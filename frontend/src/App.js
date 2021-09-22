da66446fb56999395c4ddbf7653aa2c406e1a0c9
Imran <imran.ahmed26@outlook.com>
2021-09-23 00:18:12 +0600

    added the card components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
-import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { Container } from 'react-bootstrap';
+import React from 'react'
+import {Container} from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreens from './Screens/HomeScreens'
 
 const App = () => {
   return (
     <>
-    <Header/>
-    <main className='py-3'>
-    <Container><h1>Welcome to Premium Clothing</h1></Container>
-    
-    </main>
-    <Footer/>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreens />
+        </Container>
+      </main>
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
-
+export default App
