1323d35467391d092ba2c7e180c8a233024e957a
Imran <imran.ahmed26@outlook.com>
2021-09-26 16:50:41 +0600

    added postman api

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+const router = express.Router()
+import Product from '../models/productModel.js'
+
+// @desc Fetch all products
+// @route GET /api/producs
+// @access Public..
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+
+    res.json(products)
+  })
+)
+
+// @desc Fetch single products
+// @route GET /api/producs:ID
+// @access Public..
+
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404).json({message: 'Product not found'})
+    }
+  }
+))
+
+export default router
